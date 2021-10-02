909f6d63d52723abf937a144b0d019f62e10f413
kabitamaharjan10 <kabitamaharjan10@gmail.com>
2021-10-03 00:53:07 +0545

    improvement: landing page improved

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,8 @@
    <Aboutme/>
    <!-- <PersonalInfo/> -->
    <Education/>
-   <Experiences/>
+   <!-- <Experiences/> -->
+   <Skills/>
    <Project/>
    
     </main>
@@ -14,12 +15,13 @@
 import Aboutme from 'src/components/Modules/Aboutme.vue';
 import Education from 'src/components/Modules/Education.vue';
 import Experiences from 'src/components/Modules/Experiences.vue';
-import PersonalInfo from 'src/components/Modules/PersonalInfo.vue';
-import Project from 'src/components/Modules/Project.vue';
-import { defineComponent } from 'vue';
+import Skills from 'src/components/Modules/Skills.vue';
 
-export default defineComponent({
-  components: { Aboutme, Education, Experiences, Project },
+import Project from 'src/components/Modules/Project.vue';
+
+
+export default({
+  components: { Aboutme, Education ,Skills, Project },
   name: 'PageIndex',
 });
 </script>
